1314602c3f04ddf6dc4175de04a993ca61ddbc75
unknown <denniswillie2000@gmail.com>
2021-10-27 22:53:02 +0100

    create booking class, location class, study table class, table stats class

---
+++ entities/__init__.py
@@ -0,0 +1,4 @@
+from booking import Booking
+from location import Location
+from study_table import StudyTable
+from table_stats import TableStats
---
+++ entities/table_stats.py
@@ -0,0 +1,32 @@
+class TableStats:
+    def __init__(self, table_stats_id, table_id, time, temperature_lvl, sound_lvl, co2_lvl):
+        self._table_stats_id = table_stats_id
+        self._table_id = table_id
+        self._time = time
+        self._temperature_lvl = temperature_lvl
+        self._sound_lvl = sound_lvl
+        self._co2_lvl = co2_lvl
+
+    @property
+    def table_stats_id(self):
+        return self._table_stats_id
+
+    @property
+    def table_id(self):
+        return self._table_id
+
+    @property
+    def time(self):
+        return self._time
+
+    @property
+    def temperature_lvl(self):
+        return self._temperature_lvl
+
+    @property
+    def sound_lvl(self):
+        return self._sound_lvl
+
+    @property
+    def co2_lvl(self):
+        return self._co2_lvl
