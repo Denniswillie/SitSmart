ba2b33c29702affcc1ef9ce6796282a25b339265
unknown <denniswillie2000@gmail.com>
2021-10-29 19:18:27 +0100

    remove error handling since error is handled in route handlers

--- db_managers/study_table_manager.py
+++ db_managers/study_table_manager.py
@@ -6,24 +6,17 @@ class StudyTableManager:
     def __init__(self, mysql: MySQL):
         self._mysql = mysql
 
-    # return boolean value indicating the operation is successful or not
-    def create_study_table(self, study_table: StudyTable) -> bool:
-        try:
-            cur = self._mysql.connection.cursor()
-            cur.execute("INSERT INTO studyTable(studyTableId, locationId, piMacAddress) values (%s, %s, %s)", [
-                    study_table.study_table_id,
-                    study_table.location_id,
-                    study_table.pi_mac_address
-                ]
-            )
-            self._mysql.connection.commit()
-            cur.close()
-            return True
-        except Exception as e:
-            print(e)
-            return False
+    def create_study_table(self, study_table: StudyTable):
+        cur = self._mysql.connection.cursor()
+        cur.execute("INSERT INTO studyTable(studyTableId, locationId, piMacAddress) values (%s, %s, %s)", [
+            study_table.study_table_id,
+            study_table.location_id,
+            study_table.pi_mac_address
+        ]
+                    )
+        self._mysql.connection.commit()
+        cur.close()
 
-    # Returns boolean value indicating whether the operation is successful or not
     def remove_study_table(self, study_table_id: str) -> bool:
         try:
             cur = self._mysql.connection.cursor()
