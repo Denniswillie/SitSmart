cab3cd59ccd45022dacad14ed7f0c652a80619e2
unknown <denniswillie2000@gmail.com>
2021-10-29 22:09:07 +0100

    create get_table_info function

--- db_managers/study_table_manager.py
+++ db_managers/study_table_manager.py
@@ -1,5 +1,6 @@
 from flask_mysqldb import MySQL
 from entities import StudyTable
+from entities import StudyTableInfo
 
 
 class StudyTableManager:
@@ -27,3 +28,12 @@ class StudyTableManager:
         except Exception as e:
             print(e)
             return False
+
+    def get_table_info(self, pi_mac_address: str) -> StudyTableInfo:
+        cur = self._mysql.connection.cursor()
+        cur.execute("SELECT studyTable.studyTableId, location.name from studyTable JOIN location ON "
+                    "studyTable.locationId = location.locationId WHERE piMacAddress = %s;", [pi_mac_address])
+        self._mysql.connection.commit()
+        study_table_id, location_name = cur.fetchone()
+        cur.close()
+        return StudyTableInfo(study_table_id, location_name)
