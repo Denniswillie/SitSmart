cb0e6e9ad33e8d7f4e51c7d78a0a48536b49b5b2
unknown <denniswillie2000@gmail.com>
2021-11-06 17:45:34 +0000

    export TableStatsManager

--- db_managers/__init__.py
+++ db_managers/__init__.py
@@ -1,3 +1,4 @@
 from .location_manager import LocationManager
 from .study_table_manager import StudyTableManager
 from .booking_manager import BookingManager
+from .table_stats_manager import TableStatsManager
