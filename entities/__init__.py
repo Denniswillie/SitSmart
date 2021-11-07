d7a5246a652a0a1ad0dfd8e8562cf8864317defc
unknown <denniswillie2000@gmail.com>
2021-11-07 20:08:40 +0000

    export AvailableStudyTableData class

--- entities/__init__.py
+++ entities/__init__.py
@@ -3,3 +3,4 @@ from .location import Location
 from .study_table import StudyTable
 from .table_stats import TableStats
 from .study_table_info import StudyTableInfo
+from .available_study_table_data import AvailableStudyTableData
