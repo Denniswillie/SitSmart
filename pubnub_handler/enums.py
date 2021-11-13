29cbfb210c66f38cd57ee63e30d269bf047751bd
unknown <denniswillie2000@gmail.com>
2021-11-13 15:24:51 +0000

    add enums for creating location and verifying location id

--- pubnub_handler/enums.py
+++ pubnub_handler/enums.py
@@ -3,6 +3,8 @@ import enum
 
 # Pubnub message types that will be handled when received by the subscriber
 class MessageType(enum.Enum):
+    CREATE_LOCATION = "CREATE_LOCATION"
+    VERIFY_LOCATION_ID = "VERIFY_LOCATION_ID"
     GET_TABLE_INFO = "GET_TABLE_INFO"
     CREATE_TABLE = "CREATE_TABLE"
     REMOVE_TABLE = "REMOVE_TABLE"
