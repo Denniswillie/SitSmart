ad79cbf5c498b99d9bc2ee70454293c9b2d0e834
unknown <denniswillie2000@gmail.com>
2021-11-13 15:26:13 +0000

    handle create location and verify location id

--- pubnub_handler/handler.py
+++ pubnub_handler/handler.py
@@ -1,7 +1,7 @@
 from pubnub.callbacks import SubscribeCallback
 from pubnub.pubnub import PubNub
 from pubnub_handler.enums import MessageType
-from pubnub_handler.utils import get_table_info, create_table, remove_table, save_table_stats
+from pubnub_handler.utils import get_table_info, create_table, remove_table, save_table_stats, create_location, verify_location_id
 from flask import Flask
 from entities import StudyTableInfo
 
@@ -34,14 +34,23 @@ class PubnubHandler:
                     # Need to provide an app context in order to get the mysql connection cursor.
                     with self._app.app_context():
                         if "sender" in event.message and event.message["sender"] != pubnub.uuid:
-                            if event.message["type"] == MessageType.GET_TABLE_INFO.name:
+                            if event.message["type"] == MessageType.CREATE_LOCATION.name:
+                                pubnub.publish().channel(channel).message({
+                                    "type": MessageType.CREATE_LOCATION.name,
+                                    "sender": pubnub.uuid,
+                                    "location_id": create_location(event.message, self._mysql)
+                                })
+                            elif event.message["type"] == MessageType.VERIFY_LOCATION_ID.name:
+                                pubnub.publish().channel(channel).message({
+                                    "type": MessageType.VERIFY_LOCATION_ID.name,
+                                    "sender": pubnub.uuid,
+                                    "verified": verify_location_id(event.message, self._mysql),
+                                    "location_id": event.message["location_id"]
+                                })
+                            elif event.message["type"] == MessageType.GET_TABLE_INFO.name:
                                 study_table_info = get_table_info(event.message, self._mysql)
                                 pubnub.publish().channel(channel).message({
-                                    "study_table_info": {
-                                        "study_table_id": study_table_info.study_table_id,
-                                        "study_table_name": study_table_info.study_table_name,
-                                        "location_name": study_table_info.location_name
-                                    },
+                                    "study_table_info": study_table_info.__dict__,
                                     "type": MessageType.GET_TABLE_INFO.name,
                                     "sender": pubnub.uuid
                                 }).sync()
