ff9cfeb60ee63cb6b79120f67b1f1893d4f634f9
unknown <denniswillie2000@gmail.com>
2021-11-27 10:41:49 +0000

    fsfs

--- db_managers/study_table_manager.py
+++ db_managers/study_table_manager.py
@@ -55,7 +55,7 @@ class StudyTableManager:
         cur.execute("select studyTable.studyTableId, studyTable.studyTableName, studyTable.locationId, "
                     "studyTable.piMacAddress from studyTable LEFT JOIN booking on studyTable.studyTableId = "
                     "booking.studyTableId WHERE studyTable.locationId = %s group by studyTable.studyTableId having "
-                    "count(case when not (2021-11-27 12:00:00 <= booking.startTime or booking.endTime <= 2021-11-27 09:00:00) then 1 end) = 0;",
+                    "count(case when not ('2021-11-27 12:00:00' <= booking.startTime or booking.endTime <= '2021-11-27 09:00:00') then 1 end) = 0;",
                     [location_id]
                     )
         self._mysql.connection.commit()
